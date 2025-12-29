45e2e1430442e134207aa1c16b79cd0c058f2423
Eric L <eric.low@gmail.com>
2025-12-29 13:26:44 -0800

    update with security changes

--- terraform/production/security.tf
+++ terraform/production/security.tf
@@ -27,7 +27,7 @@
       from_port   = 22
       to_port     = 22
       protocol    = "tcp"
-      cidr_blocks = [0.0.0.0/0]
+      cidr_blocks = ["0.0.0.0/0"]
       description = "Allow SSH from my IP"
     }
 
